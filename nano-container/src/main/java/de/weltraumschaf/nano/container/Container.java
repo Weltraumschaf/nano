bc60c6ddf17560abba1dc465b789311250b3a0d2
Sven Strittmatter <sven.strittmatter@iteratec.de>
2018-06-24 22:40:31 +0200

    Add logging

--- nano-container/src/main/java/de/weltraumschaf/nano/container/Container.java
+++ nano-container/src/main/java/de/weltraumschaf/nano/container/Container.java
@@ -1,27 +1,33 @@
 package de.weltraumschaf.nano.container;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @since 1.0.0
  */
 final class Container {
+    private static Logger LOG = LoggerFactory.getLogger( Container.class );
+
     private volatile boolean running = true;
 
     void start() {
-        System.out.println("Container starts...");
+        LOG.info("Container starts...");
+
         while (running) {
             try {
-                System.out.println("Container is running.");
+                LOG.info("Container is running.");
                 Thread.sleep(1_000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            } catch (final InterruptedException e) {
+                LOG.error(e.getMessage(), e);
                 return;
             }
         }
     }
 
     void stop() {
-        System.out.println("Container is stopping...");
+        LOG.info("Container is stopping...");
         running = false;
-        System.out.println("Container stopped.");
+        LOG.info("Container stopped.");
     }
 }
