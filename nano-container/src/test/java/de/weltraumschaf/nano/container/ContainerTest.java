be117463e60f85741eec1db862eff920591fb835
Sven Strittmatter <sven.strittmatter@iteratec.de>
2018-06-26 19:27:22 +0200

    More testing

---
+++ nano-container/src/test/java/de/weltraumschaf/nano/container/ContainerTest.java
@@ -0,0 +1,13 @@
+package de.weltraumschaf.nano.container;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+
+/**
+ * Tests for {@link Container}.
+ */
+public class ContainerTest {
+
+}
---
+++ nano-container/src/test/java/de/weltraumschaf/nano/container/DefaultServiceContextTest.java
@@ -0,0 +1,33 @@
+package de.weltraumschaf.nano.container;
+
+import de.weltraumschaf.nano.api.messaging.MessageBus;
+import nl.jqno.equalsverifier.EqualsVerifier;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.mock;
+
+/**
+ * Tests for {@link DefaultServiceContext}.
+ */
+public class DefaultServiceContextTest {
+
+    private final MessageBus messages = mock(MessageBus.class);
+    private final DefaultServiceContext sut = new DefaultServiceContext(messages);
+
+    @Test
+    public void equalsAndHashCode() {
+        EqualsVerifier.forClass(DefaultServiceContext.class).verify();
+    }
+
+    @Test
+    public void getMessages() {
+        assertThat(sut.getMessages(), is(messages));
+    }
+
+    @Test
+    public void string() {
+        assertThat(sut.toString(), startsWith("DefaultServiceContext(messages="));
+    }
+}
