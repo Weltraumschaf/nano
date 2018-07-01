7a4fb77eadcee80b9fa6b1d05db515894abdacaa
Sven Strittmatter <sven.strittmatter@iteratec.de>
2018-07-01 23:07:02 +0200

    Fix line length

--- nano-api/src/main/java/de/weltraumschaf/nano/api/ModuleDescriber.java
+++ nano-api/src/main/java/de/weltraumschaf/nano/api/ModuleDescriber.java
@@ -11,7 +11,9 @@ import java.util.Set;
 /**
  * Implementations describe a module.
  * <p>
- * Each Module API must contain one implementation of this interface and file {@literal META-INF/services/de.weltraumschaf.nano.api.ModuleDescriber} which contains the canonical class name of that implementation.
+ * Each Module API must contain one implementation of this interface and file
+ * {@literal META-INF/services/de.weltraumschaf.nano.api.ModuleDescriber} which contains the canonical class name of
+ * that implementation.
  * </p>
  *
  * @since 1.0.0
