502689544a11fdb28a09e6e7f03dc13d9db87a90
Sven Strittmatter <sven.strittmatter@iteratec.de>
2018-07-02 23:49:16 +0200

    USe repeater and simplify logging

--- nano-container/src/main/java/de/weltraumschaf/nano/container/ServiceLifecycleManager.java
+++ nano-container/src/main/java/de/weltraumschaf/nano/container/ServiceLifecycleManager.java
@@ -3,8 +3,13 @@ package de.weltraumschaf.nano.container;
 import de.weltraumschaf.commons.validate.Validate;
 import de.weltraumschaf.nano.api.messaging.MessageBus;
 import de.weltraumschaf.nano.api.service.AutoStartingService;
+import de.weltraumschaf.nano.api.service.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.concurrent.Callable;
 
 /**
  * Delegates the life cycle to all contained services.
@@ -34,7 +39,6 @@ final class ServiceLifecycleManager {
      * This means:
      * </p>
      * <ol>
-     * <li>inject all required dependent services</li>
      * <li>activate all services</li>
      * <li>start services marked as {@link AutoStartingService}</li>
      * </ol>
@@ -48,7 +52,14 @@ final class ServiceLifecycleManager {
     }
 
     /**
-     * Stops all services which are marked as {@link AutoStartingService auto starting}.
+     * Stops all services.
+     * <p>
+     * This means:
+     * </p>
+     * <ol>
+     * <li>stop services marked as {@link AutoStartingService}</li>
+     * <li>deactivate all services</li>
+     * </ol>
      */
     void stop() {
         autoStop();
@@ -59,10 +70,9 @@ final class ServiceLifecycleManager {
     private void activate(final MessageBus messages) {
         LOG.debug("Activating {} services ...", services.size());
         services.findAll()
-            .forEach(service -> {
-                LOG.debug("Activating service: {}.", service);
-                service.activate(new DefaultServiceContext(messages));
-            });
+            .stream()
+            .peek(service -> LOG.debug("Activating service: {}.", service))
+            .forEach(service -> service.activate(new DefaultServiceContext(messages)));
         LOG.debug("All services activated.");
     }
 
@@ -70,8 +80,8 @@ final class ServiceLifecycleManager {
         LOG.debug("Auto start services ...");
         final int count = services.findAutoStarting()
             .stream()
+            .peek(service -> LOG.debug("Auto start service: {}.", service))
             .mapToInt(service -> {
-                LOG.debug("Auto start service: {}.", service);
                 new Thread(service::start).start();
                 return 1;
             }).sum();
@@ -83,8 +93,8 @@ final class ServiceLifecycleManager {
         final int count = services.findAutoStarting()
             .stream()
             .filter(AutoStartingService::isRunning)
+            .peek(service -> LOG.debug("Auto stop service: {}.", service))
             .mapToInt(service -> {
-                LOG.debug("Auto stop service: {}.", service);
                 service.stop();
                 return 1;
             }).sum();
@@ -95,24 +105,8 @@ final class ServiceLifecycleManager {
         LOG.debug("Wait for stopping service ...");
 
         try {
-            Repeater.of(MILLIS_TO_WAIT, MAX_RETRIES).execute(() -> {
-                final int count = services.findAutoStarting()
-                    .stream()
-                    .filter(service -> !service.hasStopped())
-                    .mapToInt(service -> {
-                        LOG.debug("Service not yet stopped: {}.", service);
-                        return 1;
-                    })
-                    .sum();
-
-                if (count == 0) {
-                    LOG.debug("All services stopped.");
-                    return true;
-                } else {
-                    LOG.debug("{} services not stopped yet! Waiting {} ms ...", count, MILLIS_TO_WAIT);
-                    return false;
-                }
-            });
+            final Repeater repeater = Repeater.of(MILLIS_TO_WAIT, MAX_RETRIES);
+            repeater.execute(new ServiceStopper(services.findAutoStarting()));
             LOG.debug("All services stopped.");
         } catch (final Exception e) {
             LOG.error(e.getMessage(), e);
@@ -121,10 +115,37 @@ final class ServiceLifecycleManager {
 
     private void deactivate() {
         LOG.debug("Deactivating {} services ...", services.size());
-        services.findAll().forEach(service -> {
-            LOG.debug("Deactivating service: {}.", service);
-            service.deactivate();
-        });
+        services.findAll()
+            .stream()
+            .peek(service -> LOG.debug("Deactivating service: {}.", service))
+            .forEach(Service::deactivate);
         LOG.debug("All services deactivated.");
+    }
+
+    private static final class ServiceStopper implements Callable<Boolean> {
+        private static final Logger LOG = LoggerFactory.getLogger(ServiceStopper.class);
+        private final Collection<AutoStartingService> services;
+
+        private ServiceStopper(final Collection<AutoStartingService> services) {
+            super();
+            this.services = new ArrayList<>(Validate.notNull(services, "services"));
+        }
+
+        @Override
+        public Boolean call() {
+            final int count = services.stream()
+                .filter(service -> !service.hasStopped())
+                .peek(service -> LOG.debug("Service not yet stopped (waiting): {}.", service))
+                .mapToInt(service -> 1)
+                .sum();
+
+            if (count == 0) {
+                LOG.debug("All services stopped.");
+                return true;
+            } else {
+                LOG.debug("{} services not stopped yet! Waiting {} ms ...", count, MILLIS_TO_WAIT);
+                return false;
+            }
+        }
     }
 }
